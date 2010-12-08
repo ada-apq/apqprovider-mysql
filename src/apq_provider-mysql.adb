8505c68be2ed4987f53d98ff16c2ac91b7dae533
Marcelo Coraça de Freitas <marcelo@kow.com.br>
2010-12-08 21:12:17 -0200

    fixing up copyright notice

--- src/apq_provider-mysql.adb
+++ src/apq_provider-mysql.adb
@@ -1,27 +1,47 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                          APQ DATABASE BINDINGS                           --
+--                                                                          --
+--                           Connection Provider                            --
+--                                                                          --
+--                                 B o d y                                  --
+--                                                                          --
+--            Copyright (C) 2009 .. 2011 KOW Framework Project              --
+--                                                                          --
+--                                                                          --
+-- APQ is free software;  you can  redistribute it  and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion.  APQ is distributed in the hope that it will be useful, but WITH-  --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with APQ;  see file COPYING.  If not, write  --
+-- to  the Free Software Foundation,  59 Temple Place - Suite 330,  Boston, --
+-- MA 02111-1307, USA.                                                      --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
 
 
----------------
--- Ada Works --
----------------
+-------------------
+-- KOW Framework --
+-------------------
 with KOW_Config;
 
 ---------
 -- APQ --
 ---------
 with APQ_Provider;
-with APQ.MySQL.Client;
 
 
 package body APQ_Provider.MySQL is
-
-
-	function Main_Factory is new APQ_Provider.Generic_Connection_Factory( Connection_Type => APQ.MySQL.Client.Connection_Type );
-
-	function Factory( C : in KOW_Config.Config_File ) return APQ.Connection_Ptr is
-	begin
-		return Main_Factory( C );
-	end Factory;
-
 begin
 
 	APQ_Provider.Factory_Registry.Register_Factory( APQ.Engine_MySQL, Factory'Access );
